d0e44ed2688c425a8ec052616f5ef0ee69a3cf15
mosafari <m.safari2680@gmail.com>
2023-02-28 14:34:35 +0330

    create dictionary model

--- DjangoDictionary/Dictionary/models.py
+++ DjangoDictionary/Dictionary/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Dictionary(models.Model):
+    
+    word = models.CharField(max_length=255, unique=True)
+    meaning = models.CharField(max_length=255)
+    targetlang = models.CharField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.word
+    
