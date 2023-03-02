f8da4eaf5f62fb13ff54719345be341b530a306c
mosafari <m.safari2680@gmail.com>
2023-03-02 16:02:17 +0330

    adding src and dest to translator

--- DjangoDictionary/Dictionary/models.py
+++ DjangoDictionary/Dictionary/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 class Dictionary(models.Model):
     
-    word = models.CharField(max_length=255, unique=True)
-    meaning = models.CharField(max_length=255)
-    targetlang = models.CharField(max_length=255, unique=True)
-    pronunciation = models.CharField(max_length=255)
+    word = models.CharField(max_length=255)
+    meaning = models.CharField(max_length=255, default=None)
+    targetlang = models.CharField(max_length=255, default=None)
+    pronunciation = models.CharField(max_length=255, default=None)
+    src = models.CharField(max_length=255, default=None)
     
     def __str__(self):
         return self.word
--- DjangoDictionary/Dictionary/views.py
+++ DjangoDictionary/Dictionary/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, HttpResponse
 from  googletrans import Translator #--> does't work : fixed -> in pre-released version
 # from translate import Translator
 
+from .models import Dictionary
+
 # Create your views here.
 def home(request):
     if request.method == 'GET':
@@ -11,8 +13,18 @@ def home(request):
     if request.method == 'POST':
         lang = request.POST.get("lang", None)
         txt = request.POST.get("txt", None)
+        src = request.POST.get("src", None)
         print(lang,txt)
         translator = Translator()
-        tr = translator.translate(txt, dest=lang)
-        print(tr)
+        tr = translator.translate(txt,src=src, dest=lang)
+        try:
+            if tr.pronunciation:
+                p =tr.pronunciation
+            else:
+                p = ''
+        except:
+            None
+        obj = Dictionary.objects.create(word=txt, meaning=tr.text, targetlang=tr.dest, pronunciation=p, src=tr.src)
+        obj.save()
+        print(tr,tr.src)
         return HttpResponse(render(request, 'home.html', {"result":tr.text}), status=200)
